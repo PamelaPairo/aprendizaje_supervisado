41bd6035423ddda42f5ad854143be33143281c7b
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2021-09-17 16:10:52 -0300

    Add submission function to utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from os import makedirs
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import KBinsDiscretizer, OneHotEncoder
@@ -6,6 +7,7 @@ from sklearn.model_selection import train_test_split
 
 URL_TRAIN_DATA = "https://www.famaf.unc.edu.ar/~nocampo043/travel_insurance_prediction_train.csv"
 URL_TEST_DATA = "https://www.famaf.unc.edu.ar/~nocampo043/travel_insurance_prediction_test.csv"
+DIR_SUBMISSIONS = "submissions"
 
 df_train = pd.read_csv(URL_TRAIN_DATA)
 df_test = pd.read_csv(URL_TEST_DATA)
@@ -35,3 +37,15 @@ preprocessor = ColumnTransformer(
 make_pipeline = lambda model: Pipeline(
     [("preprocessor", preprocessor),
      ("model", model)])
+
+def save_predictions(model, filename):
+    test_id = df_test["Customer"]
+    test_pred = model.predict(df_test.drop(columns=["Customer"]))
+
+    submission = pd.DataFrame(list(zip(test_id, test_pred)),
+                              columns=["Customer", "TravelInsurance"])
+    
+    makedirs(DIR_SUBMISSIONS, exist_ok=True)
+    submission.to_csv(f"{DIR_SUBMISSIONS}/{filename}",
+                      header=True,
+                      index=False)
