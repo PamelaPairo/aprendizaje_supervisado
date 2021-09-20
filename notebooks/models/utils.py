ca77e4077188e5179665d7d0e0209acb646637b6
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2021-09-19 22:52:38 -0300

    Add comments to utils.py

--- notebooks/models/utils.py
+++ notebooks/models/utils.py
@@ -5,15 +5,19 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import KBinsDiscretizer, OneHotEncoder
 from sklearn.model_selection import train_test_split
 
+# URL of where the training and testing samples are located
 URL_TRAIN_DATA = "https://www.famaf.unc.edu.ar/~nocampo043/travel_insurance_prediction_train.csv"
 URL_TEST_DATA = "https://www.famaf.unc.edu.ar/~nocampo043/travel_insurance_prediction_test.csv"
+
+# Directory name to save submissions
 DIR_SUBMISSIONS = "submissions"
 
+# Datasets
 df_train = pd.read_csv(URL_TRAIN_DATA)
 df_test = pd.read_csv(URL_TEST_DATA)
 
+# Delete customer id and separate target labels from the features
 seed = 0
-
 X_train_total = df_train.drop(["Customer","TravelInsurance"], axis=1)
 y_train_total = df_train["TravelInsurance"]
 
@@ -23,6 +27,9 @@ X_train, X_val, y_train, y_val = train_test_split(X_train_total,
                                                   random_state=seed)
 X_test = df_test.drop("Customer", axis=1)
 
+
+# Define pipeline that discretizes columns Age and AnnaulIncome, and encode the
+# rest of variables using an ohe approach
 preprocessor = ColumnTransformer(
     [("discretizer",
       KBinsDiscretizer(n_bins=5, encode="ordinal",
@@ -34,10 +41,14 @@ preprocessor = ColumnTransformer(
       ])],
     remainder="passthrough")
 
+
+# Define a wrapper that instantiate the pipeline given a model 
 make_pipeline = lambda model: Pipeline(
     [("preprocessor", preprocessor),
      ("model", model)])
 
+# Function to save predictions in @DIR_SUBMISSIONS given a model and its
+# filename
 def save_predictions(model, filename):
     test_id = df_test["Customer"]
     test_pred = model.predict(df_test.drop(columns=["Customer"]))
